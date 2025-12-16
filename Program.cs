ef83d6c68a357feab30061999608bd469f08b175
Daisy <daisyallen89@comcast.net>
2025-12-15 20:38:51 -0500

    Add database operations
    Add database operations to demonstrate EF Core functionality
    
    - Retrieve and display all employees with their departments
    - Filter and display HR department employees
    - Add new employee to Engineering department
    - Implement Include() for eager loading of Department data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MSDF_EFCoreModelApp.Models;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new HRDbContext())
+        {
+            var allEmployees = context.Employees.Include(e => e.Department).ToList();
+            foreach (var emp in allEmployees)
+            {
+                Console.WriteLine($"{emp.FirstName} {emp.LastName} - {emp.Department?.Name ?? "N/A"}");
+            }
+
+            var hrEmployees = context.Employees
+                                      .Include(e => e.Department)
+                                      .Where(e => e.Department.Name == "HR")
+                                      .ToList();
+            Console.WriteLine("HR Department Employees:");
+            foreach (var emp in hrEmployees)
+            {
+                Console.WriteLine($"{emp.FirstName} {emp.LastName}");
+            }
+
+            var newEmployee = new Employee
+            {
+                FirstName = "New",
+                LastName = "Employee",
+                DateOfHire = DateTime.Now,
+                DepartmentID = 2
+            };
+            context.Employees.Add(newEmployee);
+            context.SaveChanges();
+
+            Console.WriteLine("New employee added.");
+        }
+    }
+}
